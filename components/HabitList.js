cb70dfa296f4e5aa3f41419d5456f89c4c879a7d
Shubham Singh <shubham88singh@gmail.com>
2020-08-23 12:58:33 +0530

    Adding the query to the client

--- components/HabitList.js
+++ components/HabitList.js
@@ -1,11 +1,24 @@
 const { default: Habit } = require('./Habit');
+import { useQuery, gql } from '@apollo/client';
 
+const HABIT_QUERY = gql`
+  query Habits {
+    habits {
+      title
+      _id
+    }
+  }
+`;
 const HabitList = ({ habits }) => {
+  const { data, error, loading } = useQuery(HABIT_QUERY);
+  if (loading) return <div>Loading...</div>;
+  if (error)
+    return <div>Error connecting to servers with response {error}</div>;
   return (
     <section>
       <h2>My Habits</h2>
-      {habits.map((habit, index) => (
-        <Habit key={habit} habit={habit} index={index} />
+      {data.habits.map((habit, index) => (
+        <Habit key={habit._id} habit={habit.title} index={index} />
       ))}
     </section>
   );
